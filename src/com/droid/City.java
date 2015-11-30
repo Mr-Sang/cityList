8773ce2559746d14dea92cae71427e9a88e6d67c
桑博 <sangxianbo@126.com>
2015-11-30 13:26:52 +0800

    初始导入

---
+++ src/com/droid/City.java
@@ -0,0 +1,22 @@
+package com.droid;
+
+public class City {
+	public String name;
+	public String pinyi;
+
+	public City(String name, String pinyi) {
+		super();
+		this.name = name;
+		this.pinyi = pinyi;
+	}
+	
+	public City(String name){
+		super();
+		this.name = name;
+	}
+
+	public City() {
+		super();
+	}
+
+}
